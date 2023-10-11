96a5c0a21466667b3cb93d16620f8bb1f6efb4f0
Aleticod <193837@unsaac.edu.pe>
2023-10-11 15:43:15 -0500

    Add params and queries request

--- index.js
+++ index.js
@@ -6,6 +6,49 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
+app.get('/products', (req, res) => {
+  const products = [
+    {
+      name: 'Product 1',
+      price: 10000,
+    },
+    {
+      name: 'Product 2',
+      price: 20000,
+    },
+  ];
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 300',
+    price: 20000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parametros')
+  }
+});
+
 app.listen(port, () => {
   console.log('Mi port es ' + port);
 });
