024d12d88337d4107406d8757999d0aa2cb3148c
Aleticod <193837@unsaac.edu.pe>
2023-10-13 15:01:10 -0500

    Add router API function to call any router for application

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routerAPI = require('./routes/index');
 const app = express();
 const port = 3000;
 
@@ -6,48 +7,27 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-app.get('/products', (req, res) => {
-  const products = [
-    {
-      name: 'Product 1',
-      price: 10000,
-    },
-    {
-      name: 'Product 2',
-      price: 20000,
-    },
-  ];
-  res.json(products);
-});
+routerAPI(app);
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 300',
-    price: 20000,
-  });
-});
+// app.get('/categories/:categoryId/products/:productId', (req, res) => {
+//   const { categoryId, productId } = req.params;
+//   res.json({
+//     categoryId,
+//     productId,
+//   });
+// });
 
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    })
-  } else {
-    res.send('No hay parametros')
-  }
-});
+// app.get('/users', (req, res) => {
+//   const { limit, offset } = req.query;
+//   if (limit && offset) {
+//     res.json({
+//       limit,
+//       offset,
+//     });
+//   } else {
+//     res.send('No hay parametros');
+//   }
+// });
 
 app.listen(port, () => {
   console.log('Mi port es ' + port);
